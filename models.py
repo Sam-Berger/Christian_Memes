116d08b3e68fee441b28ed2d742ea8c5e06e9c34
SamBATTSTL <sb631k@att.com>
2022-09-08 00:32:38 -0500

    add test comment

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 # database_path = "postgresql://postgres:postgres@localhost:5432/postgres"
+# TESTCOMMENT
 database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
   database_path = database_path.replace("postgres://", "postgresql://", 1)
